610e63c37f81fe16cb866d891ac09f21f6bb85e0
Zihao Zhang <zzh8829@gmail.com>
2022-04-28 17:05:23 -0700

    Update zeet-cli.rb

--- Formula/zeet-cli.rb
+++ Formula/zeet-cli.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class ZeetCli < Formula
   desc "Zeet CLI"
   homepage "https://github.com/zeet-dev/cli"
